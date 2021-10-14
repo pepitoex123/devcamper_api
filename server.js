7a8d3a958d4a6a6da706af3e4834214adc6311e1
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-14 20:53:29 -0300

    added basic requests to get an idea of the project, TODO: add express router

--- server.js
+++ server.js
@@ -7,6 +7,43 @@ dotenv.config({path: "./config/config.env"});
 
 const app = express();
 
+app.get("/api/v1/bootcamps", (req,res) => {
+    res.status(200).json({
+        success: true,
+        message: "Show all bootcamps!"
+    });
+})
+
+app.get("/api/v1/bootcamps/:id", (req,res) => {
+    res.status(200).json({
+        success: true,
+        message: `The bootcamp with id ${req.params.id} was sent to you`
+    })
+})
+
+app.post("/api/v1/bootcamps", (req,res) => {
+    res.status(200).json({
+        success: true,
+        message: "The new bootcamp was created!"
+    })
+})
+
+app.put("/api/v1/bootcamps/:id", (req,res) => {
+    res.status(200).json({
+        success: true,
+        message: `The bootcamp with id ${req.params.id} was updated`
+    })
+})
+
+app.delete("/api/v1/bootcamps/:id", (req,res) => {
+    res.status(200).json({
+        success: true,
+        message: `The bootcamp with id ${req.params.id} was deleted`
+    })
+})
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`));

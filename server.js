de5dee355066208b91a66a245a7bd91eb7c31bc7
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-15 19:33:48 -0300

    Successfully connected to mongoDB, added db.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-
-// Route files
-
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 // Load env vars
 
 dotenv.config({path: "./config/config.env"});
+
+
+// Connect to DB
+
+connectDB();
+
+// Route files
+
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -26,4 +31,14 @@ app.use("/api/v1/bootcamps", bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`));
+
+// Handle unhandled rejections
+
+process.on("unhandledRejection",(error,promise) => {
+    console.log(`Error: ${error.message}`);
+    // Close server and exit process
+    server.close(() => {
+        process.exit(1);
+    });
+})

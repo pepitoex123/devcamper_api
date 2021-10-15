fe1ccce2fe2606bf8ae072aecc6781307030fbdb
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-14 21:38:39 -0300

    added logging middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 
 // Route files
@@ -11,6 +12,13 @@ const bootcamps = require("./routes/bootcamps");
 dotenv.config({path: "./config/config.env"});
 
 const app = express();
+
+
+// Dev logging middleware
+
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan("dev"));
+}
 
 //  Mount routers
 

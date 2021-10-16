d8ecff0370504920416c6464a4636c8df87eeeca
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-16 18:13:33 -0300

    added custom errorHandler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -34,9 +34,7 @@ exports.getBootcamp = async(req,res,next) => {
             data: bootcamp
         })
     }catch(error){
-        res.status(400).json({
-            success: false
-        })
+        next(error);
     }
 }
 

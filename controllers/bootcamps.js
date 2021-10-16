83b4029eb80f61e81a66e1083a271d349f0965af
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-16 18:36:59 -0300

    added basic error handling with express middlewares and custom classes (ErrorResponse)

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,9 +13,7 @@ exports.getBootcamps = async(req,res,next) => {
             count: bootcamps.length
         })
     }catch(error){
-        res.status(400).json({
-            success: false
-        })
+        next(error)
     }
 }
 
@@ -33,7 +31,7 @@ exports.getBootcamp = async(req,res,next) => {
             data: bootcamp
         })
     }catch(error){
-        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404));
+        next(error)
     }
 }
 
@@ -48,9 +46,7 @@ exports.createBootcamp = async(req,res,next) => {
             data: bootcamp
         })
     }catch(error){
-        res.status(400).json({
-            success: false,
-        })
+        next(error)
     }
 }
 
@@ -64,18 +60,14 @@ exports.updateBootcamp = async(req,res,next) => {
             runValidators: true
         })
         if(!bootcamp){
-            return res.status(400).json({
-                success: false
-            })
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404));
         }
         res.status(200).json({
             success: true,
             data: bootcamp
         })
     }catch(error){
-        res.status(400).json({
-            success: false
-        })
+        next(error)
     }
 }
 
@@ -86,18 +78,14 @@ exports.deleteBootcamp = async(req,res,next) => {
     try{
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
         if(!bootcamp){
-            return res.status(400).json({
-                success: false
-            })
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404));
         }
         res.status(200).json({
             success: true,
             data: {}
         })
     }catch(error){
-        return res.status(400).json({
-            success: false
-        })
+        next(error)
     }
 }
 
--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,20 @@
+const ErrorResponse = require("./../utils/errorResponse");
+
+
 const errorHandler = (error,req,res,next) => {
+    let err = {
+        ...error
+    }
+
+    error.message = err.message;
+
+
     console.log(error.stack);
+
+    if(error.name === "CastError"){
+        const message = `Bootcamp not found with id of ${error.value}`
+        error = new ErrorResponse(message, 404);
+    }
 
     res.status(error.statusCode || 500).json({
         success: false,

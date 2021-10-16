255c2c457e113f3f3f9fcb6679675f188c7452bc
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-16 19:10:41 -0300

    improved errorHandler for duplicated fields and validation error

--- middleware/error.js
+++ middleware/error.js
@@ -11,9 +11,25 @@ const errorHandler = (error,req,res,next) => {
 
     console.log(error.stack);
 
+
+    //Mongoose bad ObjectId
     if(error.name === "CastError"){
         const message = `Bootcamp not found with id of ${error.value}`
         error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose duplicate key
+
+    if(error.code === 11000){
+        const message = "Duplicate field value entered";
+        error = new ErrorResponse(message,400);
+    }
+
+    // Mongoose validation error
+
+    if(error.name === "ValidationError"){
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message,400);
     }
 
     res.status(error.statusCode || 500).json({

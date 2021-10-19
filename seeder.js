f78e94812f58129784eb1a8c1289ca10bfee76bd
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-19 16:39:26 -0300

    added pagination, added courses Schema, added Course model to seeder.js

--- seeder.js
+++ seeder.js
@@ -11,6 +11,7 @@ dotenv.config({
 // Load models
 
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // Connect to DB
 
@@ -23,13 +24,15 @@ mongoose.connect(process.env.MONGO_URI,{
 
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`),"utf-8");
 
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`),"utf-8");
+
 
 // Import into DB
 
 const importData = async () => {
     try{
         await Bootcamp.create(bootcamps);
-
+        await Bootcamp.create(courses);
         console.log("Data imported...");
         process.exit();
     }catch(err){
@@ -42,6 +45,7 @@ const importData = async () => {
 const deleteData = async() => {
     try{
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log("Data destroyed...");
         process.exit();
     }catch(err){

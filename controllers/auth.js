009e46952bd3d0b009c8116e973ffed53bdc8b4f
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-21 09:32:26 -0300

    added basic token functionality with JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,24 @@ const asyncHandler = require("./../middleware/async");
 // @access Public
 
 exports.register = asyncHandler(async (req,res,next) => {
-    res.status(200).json({
-        success: true
+    const {name,email,password,role } = req.body;
+
+    // Create user
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
     })
+
+    // Create token
+
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true,
+        token
+    })
+
 })

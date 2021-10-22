8107910cba58e02e38ba65a98d9eaa2d21218831
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-22 14:39:12 -0300

    added relationship between Bootcamp and User, added Users to seeder.js

--- seeder.js
+++ seeder.js
@@ -12,6 +12,7 @@ dotenv.config({
 
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 // Connect to DB
 
@@ -26,13 +27,16 @@ const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`
 
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`),"utf-8");
 
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`),"utf-8");
+
 
 // Import into DB
 
 const importData = async () => {
     try{
         await Bootcamp.create(bootcamps);
-        await Bootcamp.create(courses);
+        await Course.create(courses);
+        await User.create(users);
         console.log("Data imported...");
         process.exit();
     }catch(err){
@@ -46,6 +50,7 @@ const deleteData = async() => {
     try{
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
         console.log("Data destroyed...");
         process.exit();
     }catch(err){
